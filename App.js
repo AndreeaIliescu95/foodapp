39b7b46d5a3581ce4e5281c31441cb4af3e0ae7c
Andreea Iliescu <andreeailiescu.digital@gmail.com>
2020-10-18 17:50:32 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,65 @@
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
+import './App.css';
+
+
+
+const App = () =>{
+const APP_ID = '960b1301';
+const APP_KEY= 'ae2a5ac6bbe0a5e1a82402bb271aceb1';
+
+const [recipes, setRecipes] = useState([]);
+const [search, setSearch] = useState('');
+const [query, setQuery] = useState('chicken');
+const exemplu = "andreea";
+
+useEffect(() =>{
+getRecipes();
+}, [query]);
+
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
+
+const updateSearch = e =>{
+  setSearch(e.target.value);
+
+}
+
+const getSearch = e =>{
+  e.preventDefault();
+  setQuery(search);
+  setSearch('');
+}
+  return(
+    <div className = "App">
+      <h1>{exemplu}</h1>
+      <form onSubmit={getSearch} className = "search-form">
+        <input className = "search-bar" type ="text" value = {search} onChange = {updateSearch} />
+        <button className = "search-button" type = "submit">
+          Search
+        </button>
+
+
+      </form>
+      <div className = "recipes">
+      {recipes.map(recipe => (
+        <Recipe 
+        key = {recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        
+        />
+      ))}
+      </div>  
+    </div>
+  )
+}
+export default App;
---
+++ Recipe.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './recipe.module.css';
+
+
+
+const Recipe = ({title, calories, image, ingredients}) =>{
+    return(
+        <div className={style.recipe}>
+            <h1 className ={style.calories}>{title}</h1>
+            <ul>
+                {ingredients.map(ingredient =>(
+                    <li>{ingredient.text}</li>
+                ))}
+            </ul>
+            <p className ={style.calories}> Calories: {Math.round(calories)}</p>
+            <img className={style.image}src ={image} alt=""/>
+            
+        </div>
+    );
+
+};
+
+export default Recipe;
